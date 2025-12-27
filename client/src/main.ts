951285a8735451778176630a93154c67012ad0d9
王薪林 <10655211+wang-xinlinlin@user.noreply.gitee.com>
2025-12-27 12:44:56 +0800

    feat: 初始化Vue 3 + TypeScript + Vite客户端项目

---
+++ client/src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+// import ElementAI from 'element-ai-vue'
+import 'element-plus/dist/index.css'
+// import 'element-ai-vue/dist/index.css'
+import './style.css'
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(ElementPlus)
+// app.use(ElementAI)
+app.mount('#app')
